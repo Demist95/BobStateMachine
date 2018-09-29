b3ae973efa316d8cbddc61527efa2a3455eadc61
Demis <demis_terborg@outlook.com>
2018-09-29 14:37:53 +0200

    add Observer

---
+++ Assets/Scripts/ObserverSingleton/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance = null;
+
+    public GameObject Player;
+
+    public Text YellowCounter, GreenCounter, BlueCounter, TotalCounter;
+
+    Subject subject = new Subject();
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        else if (Instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Observer yellow = new YellowCounterObserver(YellowCounter);
+        Observer green = new GreenCounterObserver(GreenCounter);
+        Observer blue = new BlueCounterObserver(BlueCounter);
+        Observer total = new TotalCounterObserver(TotalCounter);
+
+        subject.AddObserver(yellow);
+        subject.AddObserver(green);
+        subject.AddObserver(blue);
+        subject.AddObserver(total);
+    }
+
+    public void UpdateSubject(Collider collectible)
+    {
+        collectible.gameObject.SetActive(false);
+        subject.Notify(collectible);
+    }
+}
---
+++ Assets/Scripts/ObserverSingleton/Observer.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class Observer
+{
+    protected Text TextCounter;
+    protected int Count = 0;
+
+    protected Observer(Text textCounter)
+    {
+        TextCounter = textCounter;
+        UpdateText();
+    }
+
+    public abstract void OnNotify(Collider collectible);
+    protected abstract void UpdateText();
+}
+
+public class YellowCounterObserver : Observer
+{
+    public YellowCounterObserver(Text textCounter)
+        : base(textCounter){}
+
+    public override void OnNotify(Collider collectible)
+    {
+        if (collectible.gameObject.CompareTag("YellowCollectible"))
+        {
+            ++Count;
+            UpdateText();
+        }
+    }
+
+    protected override void UpdateText()
+    {
+        TextCounter.text = "Yellow: " + Count.ToString();
+    }
+}
+
+public class GreenCounterObserver : Observer
+{
+    public GreenCounterObserver(Text textCounter)
+        : base(textCounter){}
+
+    public override void OnNotify(Collider collectible)
+    {
+        if (collectible.gameObject.CompareTag("GreenCollectible"))
+        {
+            ++Count;
+            UpdateText();
+        }
+    }
+
+    protected override void UpdateText()
+    {
+        TextCounter.text = "Green: " + Count.ToString();
+    }
+}
+
+public class BlueCounterObserver : Observer
+{
+    public BlueCounterObserver(Text textCounter)
+        : base(textCounter){}
+
+    public override void OnNotify(Collider collectible)
+    {
+        if (collectible.gameObject.CompareTag("BlueCollectible"))
+        {
+            ++Count;
+            UpdateText();
+        }
+    }
+
+    protected override void UpdateText()
+    {
+        TextCounter.text = "Blue: " + Count.ToString();
+    }
+}
+
+public class TotalCounterObserver : Observer
+{
+    public TotalCounterObserver(Text textCounter)
+        : base(textCounter){}
+
+    public override void OnNotify(Collider collectible)
+    {
+        ++Count;
+        UpdateText();
+    }
+
+    protected override void UpdateText()
+    {
+        TextCounter.text = "Total: " + Count.ToString();
+    }
+}
